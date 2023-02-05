69726089282f62d0adc387cb89a2c3f970e7e620
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-02-05 21:08:55 +0100

    format

--- src/pages/api/v1/me2.ts
+++ src/pages/api/v1/me2.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const handler = async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-    console.log("api me2")
+    console.log('api me2')
     res.status(200).json({ hei: 1234 })
 }
 
