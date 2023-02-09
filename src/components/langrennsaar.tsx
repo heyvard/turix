f7ce1bce60f619c70a86a36f1613e56b855addd0
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-02-09 20:50:37 +0100

    checkboxwip

--- src/components/langrennsaar.tsx
+++ src/components/langrennsaar.tsx
@@ -1,49 +1,74 @@
-import React from 'react'
-import { UseActivities } from '../queries/useActivities'
-import dayjs from 'dayjs'
-import { Container } from '@mui/material'
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
+import React, { useState } from "react";
+import { UseActivities } from "../queries/useActivities";
+import dayjs from "dayjs";
+import { Checkbox, Container, FormControlLabel, FormGroup } from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+
 export const Langrennsaar = () => {
-    const { data: activities } = UseActivities()
+    const { data: activities } = UseActivities();
+    const [valgt, setValgt] = useState(["NordicSki"])
 
     if (!activities) {
-        return null
+        return null;
     }
 
-    const langrenn = activities.filter((a) => a.type1 == 'NordicSki' || a.type1 == 'BackcountrySki')
-    var aarStart = dayjs('2010-07-01')
+
+    const langrenn = activities.filter((a) => a.type1 == "NordicSki" || a.type1 == "BackcountrySki");
+    var aarStart = dayjs("2010-07-01");
+
+    const alletyper = activities.map((a) => a.type1);
+    const typer = alletyper.filter((n, i) => alletyper.indexOf(n) === i);
+
 
     interface Aar {
-        distance: number
-        antall: number
-        aarStart: number
-        aarSlutt: number
+        distance: number;
+        antall: number;
+        aarStart: number;
+        aarSlutt: number;
     }
-    const aarene = [] as Aar[]
+
+    const aarene = [] as Aar[];
     do {
-        const nesteAar = aarStart.add(1, 'year')
+        const nesteAar = aarStart.add(1, "year");
 
         const aktiviter = langrenn.filter((a) => {
-            let date = dayjs(a.start_date)
-            return date.isAfter(aarStart) && date.isBefore(nesteAar)
-        })
+            let date = dayjs(a.start_date);
+            return date.isAfter(aarStart) && date.isBefore(nesteAar);
+        });
 
-        const antall = aktiviter.length
-        const sum = aktiviter.map((a) => a.distance).reduce((partialSum, a) => partialSum + a, 0)
+        const antall = aktiviter.length;
+        const sum = aktiviter.map((a) => a.distance).reduce((partialSum, a) => partialSum + a, 0);
 
         const aaret: Aar = {
             antall,
             distance: sum,
             aarSlutt: nesteAar.year(),
-            aarStart: aarStart.year(),
-        }
+            aarStart: aarStart.year()
+        };
 
-        aarene.push(aaret)
-        aarStart = nesteAar
-    } while (aarStart.year() < dayjs().year())
-
+        aarene.push(aaret);
+        aarStart = nesteAar;
+    } while (aarStart.year() < dayjs().year());
     return (
         <>
+            {JSON.stringify(valgt)}
+            <FormGroup>
+                {
+                    typer.map((a) => {
+                        return <FormControlLabel key={a} control={<Checkbox onChange={(evt)=>{
+                            console.log(evt)
+                            if(evt.target.checked){
+                                console.log(a)
+                                const valgte = valgt
+                                valgte.push(a)
+                                setValgt(valgte)
+                            }
+
+                        }
+                        } />} label={a} />;
+                    })
+                }
+            </FormGroup>
             <Container maxWidth="md" sx={{ p: 0 }}>
                 <TableContainer component={Paper}>
                     <Table>
@@ -60,7 +85,7 @@ export const Langrennsaar = () => {
                                     <TableRow
                                         key={i}
                                         sx={{
-                                            '&:last-child td, &:last-child th': { border: 0 },
+                                            "&:last-child td, &:last-child th": { border: 0 }
                                         }}
                                     >
                                         <TableCell align="center" sx={{ pt: 1, pb: 2 }}>
@@ -73,12 +98,12 @@ export const Langrennsaar = () => {
                                             <Typography>{row.antall}</Typography>
                                         </TableCell>
                                     </TableRow>
-                                )
+                                );
                             })}
                         </TableBody>
                     </Table>
                 </TableContainer>
             </Container>
         </>
-    )
-}
+    );
+};
