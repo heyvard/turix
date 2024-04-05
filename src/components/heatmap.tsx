ee20fd9a060f4ccc8be9871105fc3d4d9c6b53f0
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-04-05 21:44:25 +0200

    Fjern datepicker

--- src/components/heatmap.tsx
+++ src/components/heatmap.tsx
@@ -2,22 +2,20 @@ import { useRouter } from 'next/router'
 import React from 'react'
 import { MapContainer, Polyline, Popup, TileLayer } from 'react-leaflet'
 import { LatLng } from 'leaflet'
-import dayjs from 'dayjs'
 import polyline from '@mapbox/polyline'
 import Accordion from '@mui/material/Accordion'
 import AccordionSummary from '@mui/material/AccordionSummary'
 import AccordionDetails from '@mui/material/AccordionDetails'
 import Typography from '@mui/material/Typography'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
 import { MenuItem, Select, Link as MuiLink, TextField } from '@mui/material'
 
 import { UseActivities } from '../queries/useActivities'
 import { UseUser } from '../queries/useUser'
-import { Spinner } from '../components/loading/Spinner'
 import { aktiviteter } from '../utils/aktivitetstyper'
 import { meterTilKmVisning } from '../utils/distanceUtils'
+
+import { Spinner } from './loading/Spinner'
 
 const Heatmap = () => {
     const { data: megselv } = UseUser()
@@ -28,8 +26,6 @@ const Heatmap = () => {
     const { data: activities } = UseActivities()
 
     const [aktiviteten, setAktiviteten] = React.useState(activity || 'NordicSki')
-    const [tomDato, setTomDato] = React.useState(dayjs((tom as string) || '2050-01-01'))
-    const [fomDato, setFomDato] = React.useState(dayjs((fom as string) || '2000-01-01'))
 
     if (!megselv || !activities) {
         return <Spinner></Spinner>
@@ -38,14 +34,6 @@ const Heatmap = () => {
 
     const langrennUser1 = activities
         .filter((a) => a.type1 == aktiviteten)
-        .filter((a) => {
-            const date = dayjs(a.start_date)
-            return date.isAfter(dayjs(fomDato).startOf('day'))
-        })
-        .filter((a) => {
-            const date = dayjs(a.start_date)
-            return date.isBefore(tomDato.endOf('day'))
-        })
         .map((a) => {
             const activity_polyline = a.map_summary_polyline
             const activity_name = a.name
@@ -57,49 +45,24 @@ const Heatmap = () => {
             }
         })
 
-    const tittel = `${aktiviteten} i perioden ${fomDato.format('DD.MM.YYYY')} - ${tomDato.format('DD.MM.YYYY')}`
+    const tittel = `${aktiviteten}}`
     return (
         <>
-            <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                    <Typography style={{ fontSize: '3vw' }}>{tittel}</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <Select
-                            sx={{ mb: 2 }}
-                            value={aktiviteten}
-                            label="Aktivitet"
-                            onChange={(e) => setAktiviteten(e.target.value)}
-                        >
-                            {aktiviteter.map((a) => {
-                                return (
-                                    <MenuItem key={a} value={a}>
-                                        {a}
-                                    </MenuItem>
-                                )
-                            })}
-                        </Select>
-                        <div>
-                            <DatePicker
-                                label="Fra og med dato"
-                                value={fomDato}
-                                inputFormat="DD.MM.YYYY"
-                                onChange={(newValue) => newValue && setFomDato(newValue)}
-                                renderInput={(params) => <TextField style={{ width: '8em' }} {...params} />}
-                            />
-                            <DatePicker
-                                label="Til og med dato"
-                                value={tomDato}
-                                inputFormat="DD.MM.YYYY"
-                                onChange={(newValue) => newValue && setTomDato(newValue)}
-                                renderInput={(params) => <TextField style={{ width: '8em' }} {...params} />}
-                            />
-                        </div>
-                    </LocalizationProvider>
-                </AccordionDetails>
-            </Accordion>
-            <div style={{ height: '87vh' }}>
+            <Select
+                sx={{ mt: 2, ml: 2, mb: 2 }}
+                value={aktiviteten}
+                label="Aktivitet"
+                onChange={(e) => setAktiviteten(e.target.value)}
+            >
+                {aktiviteter.map((a) => {
+                    return (
+                        <MenuItem key={a} value={a}>
+                            {a}
+                        </MenuItem>
+                    )
+                })}
+            </Select>
+            <div style={{ height: '80vh' }}>
                 <MapContainer center={position} zoom={10} style={{ height: '100%' }}>
                     <TileLayer
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
