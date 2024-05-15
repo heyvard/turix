3ed720d2b386515fe0f21b23446ccccc040edf7e
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-05-15 19:23:35 +0200

    ikoner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,8 @@ import React, { SyntheticEvent, useState } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import { Loader } from '@navikt/ds-react'
+import { Dropdown, InternalHeader, Loader } from '@navikt/ds-react'
+import { BulletListIcon, EarthIcon, HouseIcon } from '@navikt/aksel-icons'
 
 import { Spinner } from '../components/loading/Spinner'
 import { UseUser } from '../queries/useUser'
@@ -33,119 +34,23 @@ function UserFetchInnlogging(props: { children: React.ReactNode }) {
         return <Spinner />
     }
 
-    /**
-     * todo   <Menu
-     *                     id="menu-appbar"
-     *                     anchorEl={anchorEl}
-     *                     anchorOrigin={{
-     *                         vertical: 'top',
-     *                         horizontal: 'right',
-     *                     }}
-     *                     keepMounted
-     *                     transformOrigin={{
-     *                         vertical: 'bottom',
-     *                         horizontal: 'right',
-     *                     }}
-     *                     open={Boolean(anchorEl)}
-     *                     onClose={handleClose}
-     *                 >
-     *                     <MenuItem
-     *                         onClick={async () => {
-     *                             handleClose()
-     *                             router.push('/')
-     *                         }}
-     *                     >
-     *                         <Person />
-     *                         {user.displayName}
-     *                     </MenuItem>
-     *                     <MenuItem
-     *                         onClick={async () => {
-     *                             await getFirebaseAuth().signOut()
-     *                             handleClose()
-     *                         }}
-     *                     >
-     *                         <LogoutIcon />
-     *                         Logout
-     *                     </MenuItem>
-     *                 </Menu>
-     *
-     */
     return (
         <>
             {props.children}
-            <div className="fixed bottom-0 left-0 z-50 w-full h-16 bg-white border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
-                <div className="grid h-full max-w-lg grid-cols-4 mx-auto">
-                    <button
-                        type="button"
-                        onClick={() => router.push('/')}
-                        className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-                    >
-                        <svg
-                            className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                            aria-hidden="true"
-                        >
-                            <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path>
-                        </svg>
-                        <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-                            Hjem
-                        </span>
-                    </button>
-                    <button
-                        type="button"
-                        onClick={() => router.push('/heatmap')}
-                        className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-                    >
-                        <svg
-                            className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            fill="currentColor"
-                            viewBox="0 0 24 24"
-                        >
-                            <path
-                                fill-rule="evenodd"
-                                d="M5 9a7 7 0 1 1 8 6.93V21a1 1 0 1 1-2 0v-5.07A7.001 7.001 0 0 1 5 9Zm5.94-1.06A1.5 1.5 0 0 1 12 7.5a1 1 0 1 0 0-2A3.5 3.5 0 0 0 8.5 9a1 1 0 0 0 2 0c0-.398.158-.78.44-1.06Z"
-                                clip-rule="evenodd"
-                            />
-                        </svg>
-
-                        <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-                            Heatmap
-                        </span>
-                    </button>
-                    <button
-                        type="button"
-                        onClick={() => router.push('/tabell')}
-                        className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group"
-                    >
-                        <svg
-                            className="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                        >
-                            <path
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-width="2"
-                                d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5"
-                            />
-                        </svg>
-
-                        <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">
-                            Aktiviteter
-                        </span>
-                    </button>
-                </div>
-            </div>
+            <InternalHeader className="fixed bottom-0 left-0 z-50 w-full h-16 flex justify-center">
+                <InternalHeader.Button defaultChecked={true} type="button" onClick={() => router.push('/')}>
+                    <HouseIcon />
+                    Hjem
+                </InternalHeader.Button>
+                <InternalHeader.Button type="button" onClick={() => router.push('/heatmap')}>
+                    <EarthIcon />
+                    Heatmap
+                </InternalHeader.Button>
+                <InternalHeader.Button type="button" onClick={() => router.push('/tabell')}>
+                    <BulletListIcon />
+                    Aktiviteter
+                </InternalHeader.Button>
+            </InternalHeader>
         </>
     )
 }
